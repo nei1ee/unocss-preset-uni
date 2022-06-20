4957264d2defe390ab45f5fae07be32fbd1669c5
Neil <zguolee@foxmail.com>
2022-06-20 12:26:51 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import type { Preset } from '@unocss/core'
+import type { PresetMiniOptions, Theme } from '@unocss/preset-mini'
+import { rules, shortcuts, theme, variants } from '@unocss/preset-wind'
+import { preflights } from './preflights'
+import { variantColorMix } from './variants/mix'
+
+export type { Theme }
+
+export interface PresetUnoOptions extends PresetMiniOptions {}
+
+export const presetUni = (options: PresetUnoOptions = {}): Preset<Theme> => {
+  options.dark = options.dark ?? 'class'
+  options.attributifyPseudo = options.attributifyPseudo ?? false
+
+  return {
+    name: 'preset-uni',
+    theme,
+    rules,
+    shortcuts,
+    variants: [
+      ...variants(options),
+      variantColorMix,
+    ],
+    options,
+    preflights,
+  }
+}
+
+export default presetUni
